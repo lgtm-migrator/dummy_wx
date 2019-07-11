1f8e085e4dafaf542a3eaf042e2c3cfabfaa89f6
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2019-07-11 21:44:27 +0100

    Build source

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Setup script"""
+
+from setuptools import setup, find_packages
+setup(
+       name               = "dummy_wx",
+       packages           = find_packages(),
+    )
