91b438e206fa12864ff953dcc3cd65b980e34585
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2019-07-22 14:12:40 +0100

    Build source

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 
 from setuptools import setup, find_packages
 setup(
-       name               = "dummy_wx",
-       packages           = find_packages(),
+    author="Dominic Davis-Foster",
+    author_email="dominic@davis-foster.co.uk",
+    description="This module does nothing",
+    license="GPLv3",
+    long_description="This module does nothing, but us useful when trying to build documentation for modules that require wxpython.",
+    name= "dummy_wx",
+    packages=find_packages(),
+    version="0.0.1",
     )
