453abba57618658e57e8544b618befcbb9951f93
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-03-31 22:07:49 +0100

    Added py and tools modules

--- parser.py
+++ parser.py
@@ -10,23 +10,39 @@ import pathlib
 import wx
 import wx.ribbon, wx.grid, wx.dataview, wx.richtext, wx.html
 import wx.html2, wx.stc, wx.media, wx.propgrid, wx.xrc, wx.xml
-import wx.py, wx.tools, wx.adv, wx.lib, wx.aui
-from wx.lib import (
-	anchors, busy, buttons, calendar, CDate,
-	ClickableHtmlWindow, colourdb, colourselect,
-	colourutils, combotreebox, delayedresult,
-	dialogs, docview, dragscroller,	embeddedimage,
-	eventStack,	eventwatcher, evtmgr, expando,
-	fancytext, filebrowsebutton, foldmenu, gestures,
-	gridmovers, imagebrowser, imageutils, infoframe,
-	inspection, intctrl, itemspicker, langlistctrl,
-	layoutf, msgpanel, multisash, newevent, nvdlg,
-	pdfwin, platebtn, popupctl, printout, progressindicator,
-	pydocview, rcsizer, resizewidget, scrolledpanel,
-	sheet, sized_controls, softwareupdate, splitter,
-	statbmp, stattext, throbber, ticker, ticker_xrc,
-	utils, wordwrap, wxpTag,
-	)
+import wx.tools, wx.adv, wx.lib, wx.aui, wx.py
+
+import wx.tools.dbg, wx.tools.helpviewer, wx.tools.img2img, wx.tools.img2png
+import wx.tools.img2py, wx.tools.img2xpm, wx.tools.pywxrc, wx.tools.wxget
+
+import wx.py.buffer, wx.py.crust, wx.py.crustslices, wx.py.dispatcher
+import wx.py.document, wx.py.editor, wx.py.editwindow, wx.py.filling
+import wx.py.frame, wx.py.images, wx.py.interpreter, wx.py.introspect
+import wx.py.magic, wx.py.parse, wx.py.path, wx.py.pseudo, wx.py.PyAlaCarte
+import wx.py.PyAlaMode, wx.py.PyAlaModeTest, wx.py.PyCrust, wx.py.PyFilling
+import wx.py.PyShell, wx.py.PySlices, wx.py.PySlicesShell, wx.py.PyWrap
+import wx.py.shell, wx.py.sliceshell, wx.py.version
+
+import wx.lib.anchors, wx.lib.busy, wx.lib.buttons, wx.lib.calendar
+import wx.lib.ClickableHtmlWindow, wx.lib.colourdb, wx.lib.colourselect
+import wx.lib.colourutils, wx.lib.combotreebox, wx.lib.delayedresult
+import wx.lib.dialogs, wx.lib.docview, wx.lib.dragscroller, wx.lib.splitter
+import wx.lib.eventStack, wx.lib.eventwatcher, wx.lib.evtmgr, wx.lib.CDate
+import wx.lib.fancytext, wx.lib.filebrowsebutton, wx.lib.foldmenu
+import wx.lib.gridmovers, wx.lib.imagebrowser, wx.lib.imageutils, wx.lib.nvdlg
+import wx.lib.inspection, wx.lib.intctrl, wx.lib.itemspicker, wx.lib.ticker_xrc
+import wx.lib.layoutf, wx.lib.msgpanel, wx.lib.multisash, wx.lib.newevent
+import wx.lib.pdfwin, wx.lib.platebtn, wx.lib.popupctl, wx.lib.printout
+import wx.lib.pydocview, wx.lib.rcsizer, wx.lib.resizewidget
+import wx.lib.sheet, wx.lib.sized_controls, wx.lib.softwareupdate
+import wx.lib.statbmp, wx.lib.stattext, wx.lib.throbber, wx.lib.ticker
+import wx.lib.utils, wx.lib.wordwrap, wx.lib.wxpTag, wx.lib.progressindicator
+import wx.lib.langlistctrl, wx.lib.embeddedimage, wx.lib.expando
+import wx.lib.gestures, wx.lib.infoframe, wx.lib.scrolledpanel
+
+
+
+
 # Don't remove these imports: they are actually used
 
 
@@ -99,7 +115,17 @@ def parse_module(module_name):
 
 def parse_lib_submodule(submodule_name):
 	with open(f"wx/lib/{submodule_name}.py", "w") as fp:
-		parse(globals()[submodule_name], fp)
+		parse(getattr(wx.lib, submodule_name), fp)
+		
+		
+def parse_tools_submodule(submodule_name):
+	with open(f"wx/tools/{submodule_name}.py", "w") as fp:
+		parse(getattr(wx.tools, submodule_name), fp)
+		
+		
+def parse_py_submodule(submodule_name):
+	with open(f"wx/py/{submodule_name}.py", "w") as fp:
+		parse(getattr(wx.py, submodule_name), fp)
 		
 		
 with open("wx/__init__.py", "w") as fp:
@@ -151,10 +177,56 @@ for submodule in [
 	parse_lib_submodule(submodule)
 
 
+if not pathlib.Path("./wx/tools").exists():
+	pathlib.Path("./wx/tools").mkdir()
+
+with open("wx/tools/__init__.py", "w") as fp:
+	parse(wx.lib, fp)
+
+for submodule in [
+		"dbg", "helpviewer", "img2img", "img2png",
+		"img2py", "img2xpm", "pywxrc", "wxget",
+		]:
+	parse_tools_submodule(submodule)
+	
+if not pathlib.Path("./wx/py").exists():
+	pathlib.Path("./wx/py").mkdir()
+
+with open("wx/py/__init__.py", "w") as fp:
+	parse(wx.lib, fp)
+
+for submodule in [
+		"buffer", "crust", "crustslices", "dispatcher", "document",
+		"editor", "editwindow", "filling", "frame", "images",
+		"interpreter", "introspect", "magic", "parse", "path", "pseudo",
+		"PyAlaCarte", "PyAlaMode", "PyAlaModeTest", "PyCrust", "PyFilling",
+		"PyShell", "PySlices", "PySlicesShell", "PyWrap", "shell",
+		"sliceshell", "version",
+		]:
+	parse_py_submodule(submodule)
+
+
 for module in [
 		"grid",	"dataview",	"richtext",	"ribbon", "aui",
 		"html", "html2", "stc", "media", "adv",
-		"propgrid", "xrc", "xml", "py", "tools",
+		"propgrid", "xrc", "xml",
 		]:
 	parse_module(module)
 
+
+# TODO: The following in wx.lib:
+"""
+agw
+analogclock
+art
+colourchooser
+editor
+floatcanvas
+gizmos
+masked
+mixins
+ogl
+plot
+pubsub
+wxcairo
+"""
