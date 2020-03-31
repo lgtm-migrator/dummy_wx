75241bd2b78064be8d56ed74bb5c16afe1d49c63
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-03-31 20:23:25 +0100

    Switched license to LGPL to match wxWindows Licence

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Dominic Davis-Foster",
     author_email="dominic@davis-foster.co.uk",
     description="This module does nothing",
-    license="GPLv3",
+    license="LGPLv3+",
     long_description="This module does nothing, but us useful when trying to build documentation for modules that require wxpython.",
     name="dummy_wx",
     packages=find_packages(),
