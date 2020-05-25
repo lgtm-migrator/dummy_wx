647ecefe15e3ff50aaee7fede8a7087c5c1074fa
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-05-25 19:21:14 +0100

    Updated files with `git_helper`

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
+# This file is managed by `git_helper`. Don't edit it directly
 """Setup script"""
+
+from __pkginfo__ import *
 
 from setuptools import setup, find_packages
 
-description = """
-This module does nothing, but is useful when trying to build documentation
-for modules that require wxPython.
-
-Do not install this module if you already have wxPython installed, as it uses
-the same "wx" package name to pretend to be wxPython.
-
-"""
-
 setup(
-    author="Dominic Davis-Foster",
-    author_email="dominic@davis-foster.co.uk",
-    description="This module does nothing",
-    license="LGPLv3+",
-    long_description=description,
-    name="dummy_wx",
-    packages=find_packages(),
-    version="0.2.4",
-    )
+		author=author,
+		author_email=author_email,
+		classifiers=classifiers,
+		description=short_desc,
+		entry_points=entry_points,
+		extras_require=extras_require,
+		include_package_data=True,
+		install_requires=install_requires,
+		license=__license__,
+		long_description=long_description,
+		name=pypi_name,
+		packages=find_packages(exclude=("tests", "doc-source")),
+		project_urls=project_urls,
+		py_modules=py_modules,
+		python_requires=">=3.6",
+		url=web,
+		version=__version__,
+		keywords=keywords,
+
+		)
